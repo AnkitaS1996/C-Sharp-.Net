d92c498a727d48f82a218170d5395b7a62a7346d
AnkitaS1996 <ankitashedage2661@gmail.com>
2021-04-04 10:20:56 +0530

    Assignment Picture Box Use Demo

---
+++ Picture_Box_Use_Demo/Picture_Box_Use_Demo/Frm_PictureBox_Demo.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Picture_Box_Use_Demo
+{
+    partial class Frm_PictureBox_Demo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture_Box_Image = new System.Windows.Forms.PictureBox();
+            this.btn_Browse = new System.Windows.Forms.Button();
+            this.btn_Upload = new System.Windows.Forms.Button();
+            this.gb_View_Image = new System.Windows.Forms.GroupBox();
+            this.pb_Show_Image = new System.Windows.Forms.PictureBox();
+            this.txt_Show_Image = new System.Windows.Forms.TextBox();
+            this.btn_Show_Image = new System.Windows.Forms.Button();
+            this.gb_Upload_Image = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_Box_Image)).BeginInit();
+            this.gb_View_Image.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Show_Image)).BeginInit();
+            this.gb_Upload_Image.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picture_Box_Image
+            // 
+            this.picture_Box_Image.BackColor = System.Drawing.Color.Silver;
+            this.picture_Box_Image.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picture_Box_Image.Location = new System.Drawing.Point(18, 42);
+            this.picture_Box_Image.Name = "picture_Box_Image";
+            this.picture_Box_Image.Size = new System.Drawing.Size(258, 259);
+            this.picture_Box_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture_Box_Image.TabIndex = 0;
+            this.picture_Box_Image.TabStop = false;
+            // 
+            // btn_Browse
+            // 
+            this.btn_Browse.BackColor = System.Drawing.Color.LightGray;
+            this.btn_Browse.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_Browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Browse.ForeColor = System.Drawing.Color.Maroon;
+            this.btn_Browse.Location = new System.Drawing.Point(295, 42);
+            this.btn_Browse.Name = "btn_Browse";
+            this.btn_Browse.Size = new System.Drawing.Size(149, 53);
+            this.btn_Browse.TabIndex = 1;
+            this.btn_Browse.Text = "Browse";
+            this.btn_Browse.UseVisualStyleBackColor = false;
+            this.btn_Browse.Click += new System.EventHandler(this.btn_Browse_Click);
+            // 
+            // btn_Upload
+            // 
+            this.btn_Upload.BackColor = System.Drawing.Color.LightGray;
+            this.btn_Upload.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_Upload.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Upload.ForeColor = System.Drawing.Color.Maroon;
+            this.btn_Upload.Location = new System.Drawing.Point(59, 344);
+            this.btn_Upload.Name = "btn_Upload";
+            this.btn_Upload.Size = new System.Drawing.Size(162, 53);
+            this.btn_Upload.TabIndex = 2;
+            this.btn_Upload.Text = "Upload";
+            this.btn_Upload.UseVisualStyleBackColor = false;
+            this.btn_Upload.Click += new System.EventHandler(this.btn_Upload_Click);
+            // 
+            // gb_View_Image
+            // 
+            this.gb_View_Image.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gb_View_Image.Controls.Add(this.pb_Show_Image);
+            this.gb_View_Image.Controls.Add(this.txt_Show_Image);
+            this.gb_View_Image.Controls.Add(this.btn_Show_Image);
+            this.gb_View_Image.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb_View_Image.Location = new System.Drawing.Point(514, 39);
+            this.gb_View_Image.Name = "gb_View_Image";
+            this.gb_View_Image.Size = new System.Drawing.Size(419, 457);
+            this.gb_View_Image.TabIndex = 3;
+            this.gb_View_Image.TabStop = false;
+            this.gb_View_Image.Text = "View Image";
+            // 
+            // pb_Show_Image
+            // 
+            this.pb_Show_Image.BackColor = System.Drawing.Color.Silver;
+            this.pb_Show_Image.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_Show_Image.Location = new System.Drawing.Point(24, 162);
+            this.pb_Show_Image.Name = "pb_Show_Image";
+            this.pb_Show_Image.Size = new System.Drawing.Size(376, 257);
+            this.pb_Show_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Show_Image.TabIndex = 4;
+            this.pb_Show_Image.TabStop = false;
+            // 
+            // txt_Show_Image
+            // 
+            this.txt_Show_Image.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Show_Image.Location = new System.Drawing.Point(24, 63);
+            this.txt_Show_Image.Name = "txt_Show_Image";
+            this.txt_Show_Image.Size = new System.Drawing.Size(206, 38);
+            this.txt_Show_Image.TabIndex = 3;
+            // 
+            // btn_Show_Image
+            // 
+            this.btn_Show_Image.BackColor = System.Drawing.Color.LightGray;
+            this.btn_Show_Image.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_Show_Image.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Show_Image.ForeColor = System.Drawing.Color.Maroon;
+            this.btn_Show_Image.Location = new System.Drawing.Point(251, 42);
+            this.btn_Show_Image.Name = "btn_Show_Image";
+            this.btn_Show_Image.Size = new System.Drawing.Size(149, 79);
+            this.btn_Show_Image.TabIndex = 2;
+            this.btn_Show_Image.Text = "Show Image";
+            this.btn_Show_Image.UseVisualStyleBackColor = false;
+            this.btn_Show_Image.Click += new System.EventHandler(this.btn_Show_Image_Click);
+            // 
+            // gb_Upload_Image
+            // 
+            this.gb_Upload_Image.Controls.Add(this.btn_Upload);
+            this.gb_Upload_Image.Controls.Add(this.btn_Browse);
+            this.gb_Upload_Image.Controls.Add(this.picture_Box_Image);
+            this.gb_Upload_Image.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb_Upload_Image.Location = new System.Drawing.Point(25, 39);
+            this.gb_Upload_Image.Name = "gb_Upload_Image";
+            this.gb_Upload_Image.Size = new System.Drawing.Size(460, 457);
+            this.gb_Upload_Image.TabIndex = 4;
+            this.gb_Upload_Image.TabStop = false;
+            this.gb_Upload_Image.Text = "Upload Image";
+            // 
+            // Frm_PictureBox_Demo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(984, 522);
+            this.Controls.Add(this.gb_Upload_Image);
+            this.Controls.Add(this.gb_View_Image);
+            this.Name = "Frm_PictureBox_Demo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture_Box_Image)).EndInit();
+            this.gb_View_Image.ResumeLayout(false);
+            this.gb_View_Image.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Show_Image)).EndInit();
+            this.gb_Upload_Image.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture_Box_Image;
+        private System.Windows.Forms.Button btn_Browse;
+        private System.Windows.Forms.Button btn_Upload;
+        private System.Windows.Forms.GroupBox gb_View_Image;
+        private System.Windows.Forms.PictureBox pb_Show_Image;
+        private System.Windows.Forms.TextBox txt_Show_Image;
+        private System.Windows.Forms.Button btn_Show_Image;
+        private System.Windows.Forms.GroupBox gb_Upload_Image;
+    }
+}
+
---
+++ Picture_Box_Use_Demo/Picture_Box_Use_Demo/Frm_PictureBox_Demo.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Picture_Box_Use_Demo
+{
+    public partial class Frm_PictureBox_Demo : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog=Assignment_Demo_Picture_Box_db;Integrated Security=True");
+
+        void Con_Open()
+        {
+            if(con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+        void Con_Close()
+        {
+            if(con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+        public Frm_PictureBox_Demo()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Browse_Click(object sender, EventArgs e)
+        {
+            Con_Open();
+            try
+            {
+                OpenFileDialog OFD = new OpenFileDialog();
+                OFD.Filter = "Image Files(*.jpg; *.jpeg;) | *.jpg; *.jpeg;";
+
+                if(OFD.ShowDialog() == DialogResult.OK)
+                {
+                    picture_Box_Image.Image = new Bitmap(OFD.FileName);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Con_Close();
+        }
+
+        private void btn_Upload_Click(object sender, EventArgs e)
+        {
+            Con_Open();
+            try
+            {
+                ImageConverter IC = new ImageConverter();
+                byte[] ImageArray= (byte[]) IC.ConvertTo(picture_Box_Image.Image,typeof(byte[]));
+
+                SqlCommand cmd= new SqlCommand("Insert into Image_db (Image) Values (@image)",con);      //(@image is Parameter)
+                cmd.Parameters.Add("@image", SqlDbType.Image).Value = ImageArray;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Image Upload Successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                picture_Box_Image.Image = null;
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Con_Close();
+        }
+
+        private void btn_Show_Image_Click(object sender, EventArgs e)
+        {
+            Con_Open();
+            SqlCommand cmd = new SqlCommand("Select (Image) from Image_db where Image_ID = " + txt_Show_Image.Text + "", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                byte[] img = new byte[0];
+                img = (byte[])(dr["Image"]);
+                MemoryStream ms = new MemoryStream(img);
+                img = (byte[])(dr["Image"]);
+                //pb_Show_Image.Image = new Bitmap(Convert.ToByte(ms));
+                pb_Show_Image.Image = Image.FromStream(ms);
+                MessageBox.Show("Image Show Successfully", "Success", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Invalid Image ID","Please Valid Image ID",MessageBoxButtons.OK,MessageBoxIcon.Warning);         
+            }
+            txt_Show_Image.Clear();
+
+            Con_Close();
+
+           
+           /*Con_Open();
+           MemoryStream stream = new MemoryStream();
+           SqlCommand cmd = new SqlCommand("Select (Image) from Image_db where Image_ID = " + txt_Show_Image.Text  + "", con);
+           SqlDataAdapter sda = new SqlDataAdapter(cmd);
+           DataSet ds = new DataSet();
+           sda.Fill(ds);
+           
+           if(ds.Tables[0].Rows.Count > 0)
+           {
+               MemoryStream ms = new MemoryStream((byte[])ds.Tables[0].Rows[0]["Image"]);
+               pb_Show_Image.Image = Image.FromStream(ms);
+               txt_Show_Image.Clear();
+           }
+           else
+           {
+               MessageBox.Show("Invalid Image ID...!!!");
+               txt_Show_Image.Clear();
+           }
+           Con_Close();*/
+           
+
+        }
+    }
+}
